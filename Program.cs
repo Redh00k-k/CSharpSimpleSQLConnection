d946c3450112249da9048e83492875e55c1a2e07
Redh00k-k <jack.frost.6581@gmail.com>
2024-08-31 00:12:25 +0900

    [add] add options user, password and query

--- Program.cs
+++ Program.cs
@@ -19,8 +19,19 @@ class Options {
     [Option('d', "database", Required = true, HelpText = "Database name.")]
     public string database { get; set; }
 
+    [Option('q', "query", Required = false, HelpText = "Execute oneline query.")]
+    public string one_query { get; set; }
+
+    [Option('u', "user", Required = false, Default = "", HelpText = "User name.")]
+    public string user { get; set; }
+
+    [Option('p', "password", Required = false, Default = "", HelpText = "Password.")]
+    public string password { get; set; }
+
+
     [Option('n', "no-password", Required = false, HelpText = "Don't ask for password")]
     public bool no_pass { get; set; }
+
 
     [Option('h', "help", Required = false, HelpText = "Display this help screen.")]
     public bool help { get; set; }
@@ -39,16 +50,59 @@ namespace SQL {
             }
         }
 
+        static void ExecuteQueryAndShowResults(SqlConnection con, string query) {
+            try {
+                SqlCommand command = new SqlCommand(query, con);
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Gets the values
+                int row_num = 0;
+                while (reader.Read()) {
+                    Console.WriteLine($"----{row_num} row----");
+                    for (int col = 0; col < reader.FieldCount; col++) {
+                        reader.GetValue(col).GetType();
+                        Console.Write(reader.GetName(col).ToString() + ": ");
+                        if (reader.IsDBNull(col)) {
+                            Console.WriteLine("NULL");
+                        } else if (typeof(System.Byte[]) == reader.GetValue(col).GetType()) {
+                            Console.WriteLine(BitConverter.ToString(ObjectToByteArray(reader.GetValue(col))).Replace("-", " ") + " <--- CyberChef 'From Hex'");
+                        } else {
+                            Console.WriteLine(reader.GetValue(col).ToString());
+                        }
+                    }
+                    Console.WriteLine("");
+
+                    row_num++;
+                }
+                reader.Close();
+            } catch (Exception e) {
+                Console.WriteLine($"Something wrong. {e.Message}");
+            }
+        }
+
         static void Run(Options opt) {
             String conString = "";
             if (opt.no_pass) {
                 conString = "Server = " + opt.server + "; Database = " + opt.database + "; Integrated Security = True;";
             } else {
-                Console.Write("User: ");
-                string user = Console.ReadLine();
+                string user = "";
+                if (opt.user.Length != 0) { 
+                    user = opt.user;
+                    Console.WriteLine("User: " + user);
+                } else {
+                    Console.Write("User: ");
+                    user = Console.ReadLine();
+                }
 
-                Console.Write("Password: ");
-                string password = Console.ReadLine();
+                string password = "";
+                if (opt.password.Length != 0) {
+                    password = opt.password;
+                    Console.WriteLine("Password: " + password);
+                } else {
+                    Console.Write("Password: ");
+                    password = Console.ReadLine();
+                }
+
                 conString = "Server = " + opt.server + "; Database = " + opt.database + "; User ID = " + user + "; Password = " + password + ";";
             }
             Console.WriteLine(conString);
@@ -63,6 +117,13 @@ namespace SQL {
                 Environment.Exit(0);
             }
 
+            if (opt.one_query.Length != 0) {
+                ExecuteQueryAndShowResults(con, opt.one_query);
+                con.Close();
+
+                return;
+            }
+
             // send query and receive response.
             string query = "";
             do {
@@ -72,36 +133,13 @@ namespace SQL {
                     Console.WriteLine("Exiting ...");
                     break;
                 }
-                try {
-                    SqlCommand command = new SqlCommand(query, con);
-                    SqlDataReader reader = command.ExecuteReader();
 
+                ExecuteQueryAndShowResults(con, query);
 
-                    // Gets the values
-                    int row_num = 0;
-                    while (reader.Read()) {
-                        Console.WriteLine($"----{row_num} row----");
-                        for (int col = 0; col < reader.FieldCount; col++) {
-                            reader.GetValue(col).GetType();
-                            Console.Write(reader.GetName(col).ToString() + ": ");
-                            if (reader.IsDBNull(col)) {
-                                Console.WriteLine("NULL");
-                            } else if (typeof(System.Byte[]) == reader.GetValue(col).GetType()) {
-                                Console.WriteLine(System.Text.Encoding.Default.GetString(ObjectToByteArray(reader.GetValue(col))));
-                            } else {
-                                Console.WriteLine(reader.GetValue(col).ToString());
-                            }
-                        }
-                        Console.WriteLine("");
-
-                        row_num++;
-                    }
-                    reader.Close();
-                } catch (Exception e) {
-                    Console.WriteLine($"Something wrong. {e.Message}");
-                }
             } while (query != null) ;
             con.Close();
+
+            return;
         }
 
         static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errs) {
